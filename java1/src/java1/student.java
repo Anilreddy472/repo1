0523bc2775c1caac244c044099bd93777dd6bb44
Anilreddy472 <anilsuram39@gmail.com>
2021-12-22 21:41:17 +0530

    test message

---
+++ java1/src/java1/student.java
@@ -0,0 +1,27 @@
+package java1;
+
+class student
+{
+int id;
+String name;
+public class student3 {
+
+	public static void main(String[] args) {
+		 //Creating objects  
+		  student s1=new student();  
+		  student s2=new student();
+		  //Initializing objects  
+		  s1.id=101;  
+		  s1.name="Sonoo";  
+		  s2.id=102;  
+		  s2.name="Amit";  
+		  //Printing data  
+		  System.out.println(s1.id+" "+s1.name);  
+		  System.out.println(s2.id+" "+s2.name);  
+		 }  
+		}  
+
+
+	}
+
+
